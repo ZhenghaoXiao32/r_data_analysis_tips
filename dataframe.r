9a0cd2c0f3dceb60225ddf6da2d79cbb1b5f0910
Zhenghao Xiao <42816782+zhx32@users.noreply.github.com>
2019-08-04 20:04:23 -0400

    Update dataframe.r

--- dataframe.r
+++ dataframe.r
@@ -11,3 +11,8 @@ factor_cols <- c("A", "C", "E", "G")
 data %>% mutate_at(factor_cols, factor)
 str(data)
 
+### Calculate and show missing data percent per column
+data %>% 
+  summarize_all(list( missing_percent = ~sum(is.na(.)) / length(.))) %>%
+  ### if the data frame is too wide, then use gather to transform
+  gather(col, value) 
